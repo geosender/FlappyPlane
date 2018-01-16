d8cb61935a4089d334bcc094d27d44d4a17af3c8
sndg0104 <sndg0104@humbermail.ca>
2018-01-15 20:00:27 -0500

    Flappy Plane
    My Javascript app in Android Studios

---
+++ app/src/main/java/com/badlogic/androidgames/mrnom/Bomber.java
@@ -0,0 +1,16 @@
+package com.badlogic.androidgames.mrnom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Geoffrey on 4/20/2015.
+ */
+public class Bomber {
+    public int x, y;
+
+    public Bomber(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
